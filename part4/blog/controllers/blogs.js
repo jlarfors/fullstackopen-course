0a0d258f4cecef97e554b1d34d611bab88ec09a6
Jacob Larfors <jlarfors@verifa.io>
2019-12-23 10:25:57 +0200

    completed 4.8

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -2,13 +2,12 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs.map(blog => blog.toJSON()))
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
   const blog = new Blog({
@@ -17,11 +16,12 @@ blogsRouter.post('/', (request, response, next) => {
     date: new Date(),
   })
 
-  blog.save()
-    .then(savedBlog => {
-      response.json(savedBlog.toJSON())
-    })
-    .catch(error => next(error))
+  try {
+    const savedBlog = await blog.save()
+    response.json(savedBlog.toJSON())
+  } catch(exception) {
+    next(exception)
+  }
 })
 
 module.exports = blogsRouter
